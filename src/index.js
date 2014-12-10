2527ec91165b0f65ec61a5e0ad4d23109d1f7378
Romain Prieto <choicesmade@gmail.com>
2014-12-10 23:18:46 +1100

    Simplify proxy server setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-var http = require('http');
 var nock = require('nock');
 var url  = require('url');
 var httpProxy = require('http-proxy');
@@ -9,20 +8,15 @@ var PROXY_COUNT = 0;
 module.exports = Stub;
 
 function Stub(opts) {
-  var self = this;
   this.host = PROXY_HOST + (++PROXY_COUNT);
   this.port = opts.port;
   this.stub = nock('http://' + this.host + ':9999');
-  this.proxy = httpProxy.createProxyServer({
+  this.server = httpProxy.createProxyServer({
     target: 'http://' + this.host + ':9999'
-  });
-  this.server = http.createServer(function(req, res, next) {
-    self.proxy.web(req, res, {});
   });
 }
 
 Stub.prototype.start = function(done) {
-  console.log('Starting local server: http://localhost:' + this.port);
   this.server.listen(this.port, done);
 };
 
